aa4432caaa529535c83640db4348e0e36c5eb456
Amine Abdz <mimminou@gmail.com>
2022-02-28 18:05:05 +0100

    proto completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const logo = document.querySelector(".Navbar_logo_container");
+
+const heatCard = document.getElementById('heatCard');
+const coldCard = document.getElementById('coldCard');
+const acCard = document.getElementById('acCard');
+const installCard = document.getElementById('installCard');
+
+
+const cards = [heatCard,coldCard,acCard,installCard] ;
+
+logo.addEventListener("click", function(){
+    window.location.href="/";
+});
+
+
+for(let i=0; i<cards.length; i++){
+    cards[i].addEventListener("click", function(){
+        window.location.href="/services/#" + cards[i].dataset.section;
+    });
+}
--- services/services.js
+++ services/services.js
@@ -1,32 +1,41 @@
-const serv_Description = document.querySelector(".serv_description");
-// const chauffage_Button = document.querySelector("heat_button");
-// const froid_button = document.querySelector("cold_button");
-// const clim_button = document.querySelector("ac_button");
-// const install_button = document.querySelector("installation_button");
+const logo = document.querySelector(".Navbar_logo_container");
 
 //details sections :
-const chauff_section = document.querySelector("chauff_section");
-const cold_section = document.querySelector("refr_section");
-const clim_section = document.querySelector("clim_section");
-const install_section = document.querySelector("install_section");
+const chauff_section = document.querySelector(".chauff_section");
+const cold_section = document.querySelector(".refr_section");
+const clim_section = document.querySelector(".clim_section");
+const install_section = document.querySelector(".install_section");
 
+
+logo.addEventListener("click", function(){
+    window.location.href="/";
+});
 
 
 const btns = document.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
         //Add function here
-        if(btns[i].id==="heat_btn"){
-            
-        }
-        if(btns[i].id=="cold_btn"){
+        if(btns[i].id=="heat_btn"){
+            chauff_section.scrollIntoView({
+                behavior : "smooth"
+            });
 
         }
-        if(btns[i].id=="ac_btn"){
-            
+        if(btns[i].id=="cold_btn"){
+            refr_section.scrollIntoView({
+                behavior : "smooth"
+            });
         }
-        if(btns[i].id=="insall_btn"){
-            
+        if(btns[i].id=="ac_btn"){
+            clim_section.scrollIntoView({
+                behavior : "smooth"
+            });
+        }
+        if(btns[i].id=="install_btn"){
+            install_section.scrollIntoView({
+                behavior : "smooth"
+            });
         }
     });
 }
