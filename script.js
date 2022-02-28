381cfb8ef320163f73cc5f94f2b1ba318d1220b5
Amine Abdz <mimminou@gmail.com>
2022-02-28 21:45:37 +0100

    updated broken link in logo

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const installCard = document.getElementById('installCard');
 const cards = [heatCard,coldCard,acCard,installCard] ;
 
 logo.addEventListener("click", function(){
-    window.location.href="/";
+    window.location.href="../index.html";
 });
 
 
 for(let i=0; i<cards.length; i++){
     cards[i].addEventListener("click", function(){
-        window.location.href="/services/#" + cards[i].dataset.section;
+        window.location.href="./services/#" + cards[i].dataset.section;
     });
 }
